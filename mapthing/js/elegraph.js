afdb6d7a2f3ad58df7e39495c0a95117aa8312c5
Chris Dent <cdent@matter.local>
2009-02-27 05:19:04 +0000

    put the elevation data in as a profile image
    next:
    
    * zoom the profile when the map zooms

--- mapthing/js/elegraph.js
+++ mapthing/js/elegraph.js
@@ -1,57 +1,43 @@
 
 function load() {
-    var altitudes = [];
     var markers = [];
-    var dirs = [];
+    var maxNodes = 326;
 
-    function drawProfile() {
+    function drawProfile(altitudes) {
         var min=Infinity, max=0;
         // ==== some Google Chart parameters ====
-        var url = "http://chart.apis.google.com/chart?cht=ls&amp;chs=500x100&amp;chco=000000&amp;chm=B,33cc33,0,0,0&amp;chd=t:"
+        var url = "http://chart.apis.google.com/chart?cht=ls&amp;chs=1000x100&amp;chco=000000&amp;chm=B,33cc33,0,0,0&amp;chd=t:"
+        var multiplier = altitudes.length/maxNodes;
+        multiplier = parseInt(multiplier);
         for (var i=0; i<maxNodes; i++) {
+            index = (i * multiplier);
             // == filter out bogus values ==
-            if (altitudes[i] < -10000000000) altitudes[i] = 0;
+            if (altitudes[index] < -10000000000) altitudes[index] = 0;
             // == find min and max values ==
-            if (altitudes[i]<min) min=altitudes[i];
-            if (altitudes[i]>max) max=altitudes[i];
+            if (altitudes[index] < min) { min = altitudes[index]; }
+            if (altitudes[index] > max) { max = altitudes[index]; }
             // == add to the Chart URL ==
-            url += altitudes[i];
+            url += altitudes[index];
             if (i<maxNodes-1) url += ",";
         }
         // == add min/max values to Chart URL ==
         url += "&amp;chds="+min+","+max;
         // == create the Google Chart image ==
-        document.getElementById("profile").innerHTML = '<img src="' +url+ '" width=500 height=100 >';
+        document.getElementById("profile").innerHTML = '<img src="' +url+ '" width=1000 height=100 >';
     }
 
-
-    function getAltitude(poly, i) {
-        var point = poly.GetPointAtDistance(i*poly.Distance()/maxNodes);
-        GDownloadUrl('/proxy?site=alt&lat=' +point.lat()+ '&lng=' +point.lng(),
-                function(data) {
-                    var doc = GXml.parse(data);
-                    altitudes[i] = parseInt(GXml.value(doc));
-                    nodes = nodes+1;
-                    if (nodes == maxNodes) {
-                        drawProfile();
-                    }
-        });
-    }
 
     function establishRoute(map) {
         GDownloadUrl('/data/latlongelv.csv', function(data) {
                 lines = data.split("\n");
+                var altitudes = [];
                 for (var i = 0; i < lines.length; i++) {
                     info = lines[i].split(',');
                     markers[i] =  new GMarker(new GLatLng(info[0], info[1]), {title:info[2]});
                     map.addOverlay(markers[i]);
-                    altitudes[i] = info[2];
-                    /* if(i > 0) {
-                        dirs[i] = new GDirections();
-                        dirs[i].loadFromWaypoints([markers[i-1].getLatLng().toUrlValue(), markers[i].getLatLng().toUrlValue()]);
-//                        map.addOverlay(dirs[i].getPolyline());
-                    } */
+                    altitudes[i] = parseFloat(parseFloat(info[2]).toFixed(0));
                 }
+                drawProfile(altitudes);
         });
     }
 
@@ -60,7 +46,6 @@ function load() {
         var map = new GMap2(document.getElementById("map"));
         map.setCenter(new GLatLng(36, -100), 4);
         map.addControl(new GLargeMapControl());
-
         establishRoute(map);
     }
 }
